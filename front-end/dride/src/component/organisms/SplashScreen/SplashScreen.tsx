070e093a054ae3e7a544086f6914d9c035c0e0a6
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-09 22:13:40 +0200

    Add 'SplashScreen' component

---
+++ front-end/dride/src/component/organisms/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,27 @@
+import { Image, Box, Overlay, Highlight } from "@mantine/core"
+
+import imgUrl from '../../../assets/splash_background.jpg';
+
+export const SplashScreen = () => {
+
+    return (
+        <>
+            <Highlight
+                sx={{ background: 'transparent' }}
+                align="center"
+                highlight={['dRide']}
+                highlightStyles={(theme) => ({
+                    backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
+                    fontWeight: 700,
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                })}>
+                dRide | A decentralized car-sharing system using smart contracts.
+            </Highlight>
+            <Box sx={{ position: 'relative' }}>
+                <Overlay opacity={0.4} color="#000" />
+                <Image radius="md" src={imgUrl} alt="Splash Screen" />
+            </Box>
+        </>
+    );
+}
