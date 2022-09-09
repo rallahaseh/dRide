2f39aab49862b06d45777c9ff0e10435fa443c82
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-09 17:30:21 +0200

    Add wallet installation check

--- front-end/dride/src/App.tsx
+++ front-end/dride/src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Container, Header, Title } from '@mantine/core';
+import { WalletInstallation } from './component/organisms/Wallet/Installation';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { ethereum } = window as any;
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header
+        height={60}
+        px="xl"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+        <Title>dRide</Title>
+      </Header>
+      {!ethereum ? (
+        <Container p="lg">
+          <WalletInstallation />
+        </Container>
+      ) : null}
     </div>
   )
 }
