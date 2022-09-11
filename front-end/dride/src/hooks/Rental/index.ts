67785e169fbb327c13d26faa741e409c68f74801
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-12 01:14:56 +0200

    Add rental hooks

---
+++ front-end/dride/src/hooks/Rental/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @dev Rental.sol contract
+ * 
+ *  - Rent process Functionalities
+ */
+
+export { useAddRentPeriod } from './Rental';
+export { useAddInsurancePackage } from './Rental';
+export { useVerifyRentalProcess } from './Rental';
+export { useCompleteRentalProcess  } from './Rental';
