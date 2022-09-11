a0ba66b526a1b0493115619fa40ad437f9e846f0
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-12 01:02:18 +0200

    Add access hooks

---
+++ front-end/dride/src/hooks/Access.ts/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @dev Access.sol contract
+ * 
+ *  - Access Functionalities
+ */
+
+// Renter
+export { useAddOwner } from './Access';
+export { useRemoveOwner } from './Access';
+
+// Owner
+export { useAddRenter } from './Access';
+export { useRemoveRenter } from './Access';
