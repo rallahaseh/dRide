6c8adc30598f40cad1eaf799b8a2e3a525a99cf5
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-11 19:38:42 +0200

    Update hardhat configurations

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,19 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+require("@nomiclabs/hardhat-ethers");
+const { GOERLI_API_URL, DRIDE_PRIVATE_KEY } = process.env;
+
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
 module.exports = {
-  solidity: "0.8.9",
-};
+   solidity: "0.8.16",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: GOERLI_API_URL,
+         accounts: [`0x${DRIDE_PRIVATE_KEY}`]
+      }
+   },
+}
