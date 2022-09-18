ba0c81860ebfd98b89fcb35b632d5a45b550016a
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-18 19:45:41 +0200

    Initiate RentableVehicles contract

---
+++ truffle/contracts/RentableVehicles.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./ERC4907.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract RentableVehicles is ERC4907 {
+  constructor() public {
+  }
+}
