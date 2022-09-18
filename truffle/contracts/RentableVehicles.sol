e488c6c958bfa7e73bd4e707baf68fe12dbba328
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-18 20:38:09 +0200

    Write an NFT that utilizes the ERC4907 contract

--- truffle/contracts/RentableVehicles.sol
+++ truffle/contracts/RentableVehicles.sol
@@ -5,6 +5,27 @@ import "./ERC4907.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract RentableVehicles is ERC4907 {
-  constructor() public {
-  }
+    /// @dev Intiate counter to refer to NFT as IDs.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC4907("RentableVehicles", "RV") {}
+
+    /**
+     * @dev Minting an NFT refers to converting digital files into crypto collections
+     * or digital assets stored on the blockchain.
+     */
+    function mint(string memory _tokenURI) public {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+    }
+
+    /**
+     * @dev Burning a non-fungible token means destroying it.
+     */
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
 }
