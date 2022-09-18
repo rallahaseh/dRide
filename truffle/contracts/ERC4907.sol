6ee68ea0239bd0e4e1c6ed836008bc14bca1f546
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-18 19:29:00 +0200

    Add the functionality to assign an NFT to a user

--- truffle/contracts/ERC4907.sol
+++ truffle/contracts/ERC4907.sol
@@ -12,3 +12,24 @@ contract ERC4907 is ERC721URIStorage, IERC4907 {
     }
 
     mapping(uint256 => UserInfo) internal _users;
+
+    /// @notice Configure the user and the expiration date of the NFT.
+    /// @dev Returning a zero address indicates that there is no user.
+    /// Throws an error if the 'tokenId' is not valid NFT.
+    /// @param tokenId  Token id of the new user NFT
+    /// @param user  Address of the new user
+    /// @param expires  Timestamp, the new user could use the NFT before expires
+    function setUser(
+        uint256 tokenId,
+        address user,
+        uint64 expires
+    ) public virtual override {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        UserInfo storage info = _users[tokenId];
+        info.user = user;
+        info.expires = expires;
+        emit UpdateUser(tokenId, user, expires);
+    }
