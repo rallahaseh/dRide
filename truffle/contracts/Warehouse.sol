3e1e8c1864db4c319a206ae7ecaefe5ef05b7d2e
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-21 21:27:51 +0200

    Inheriting from `ReentrancyGuard`
    Which helps prevent reentrant calls to a function

--- truffle/contracts/Warehouse.sol
+++ truffle/contracts/Warehouse.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Warehouse {
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Warehouse is ReentrancyGuard {
   constructor() public {
   }
 }
