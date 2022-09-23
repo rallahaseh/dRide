08162cc916b67c895ee1ad63281becd9adae1984
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-23 19:25:25 +0200

    Add a list of helpers

--- truffle/contracts/Warehouse.sol
+++ truffle/contracts/Warehouse.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/interfaces/IERC165.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "./IERC4907.sol";
 
 contract Warehouse is ReentrancyGuard {
     struct Listing {
@@ -18,6 +23,9 @@ contract Warehouse is ReentrancyGuard {
     mapping(address => mapping(uint256 => Listing)) private _listingMap;
 
     // Warehouse Owner
+    using Counters for Counters.Counter;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.UintSet;
     address private _warehouseOwner;
 
     constructor() {
