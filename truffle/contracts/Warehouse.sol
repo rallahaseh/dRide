ac9f3a29991bbb9ea583586b91a2d3d0c35a2bfb
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-23 18:40:19 +0200

    Initiate 'Listing' object

--- truffle/contracts/Warehouse.sol
+++ truffle/contracts/Warehouse.sol
@@ -4,8 +4,19 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Warehouse is ReentrancyGuard {
-  constructor() public {
-  }
+    struct Listing {
+        address owner;
+        address renter;
+        address nftContract; // NFT contract address
+        uint256 tokenId; // NFT id
+        uint256 pricePerDay; // Price
+        uint256 startDateUNIX; // Rental start date
+        uint256 endDateUNIX; // Rental end date
+        uint256 expires; // Rental expiry date
+    }
+    // Translates contract address to token id, which is then mapped to the features of the rental listing
+    mapping(address => mapping(uint256 => Listing)) private _listingMap;
+
     // Warehouse Owner
     address private _warehouseOwner;
 
