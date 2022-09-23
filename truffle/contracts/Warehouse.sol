f6fc2167575f18a49900442b320096bd7d3beacf
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-09-23 19:27:02 +0200

    Add list of modifiers will be used for listNFT fun

--- truffle/contracts/Warehouse.sol
+++ truffle/contracts/Warehouse.sol
@@ -26,8 +26,34 @@ contract Warehouse is ReentrancyGuard {
     using Counters for Counters.Counter;
     using EnumerableSet for EnumerableSet.AddressSet;
     using EnumerableSet for EnumerableSet.UintSet;
+
+    /// Variables
+    Counters.Counter private _nftsListed;
     address private _warehouseOwner;
 
+    /// Modifiers
+    modifier ownerOfIERC721(address nftContract, uint256 tokenId) {
+        require(
+            IERC721(nftContract).ownerOf(tokenId) == msg.sender,
+            "Not owner of nft"
+        );
+        _;
+    }
+    modifier correctPrice(uint256 price) {
+        require(price > 0, "Rental price should be greater than 0");
+        _;
+    }
+    modifier validateStartDate(uint256 date) {
+        require(date >= block.timestamp, "Start date cannot be in the past");
+        _;
+    }
+    modifier validateEndDate(uint256 startDate, uint256 endDate) {
+        require(
+            endDate >= startDate,
+            "End date cannot be before the start date"
+        );
+        _;
+    }
     constructor() {
         _warehouseOwner = msg.sender;
     }
