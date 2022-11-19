a1a3e4db4c6229927a005547c71afd7d0d21bc4b
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-11-19 01:39:03 +0100

    Update contract params

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -2,7 +2,7 @@ const RentableVehicles = artifacts.require("RentableVehicles");
 const Marketplace = artifacts.require("Marketplace");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(Marketplace);
+  await deployer.deploy(Marketplace, "0x07865c6E87B9F70255377e024ace6630C1Eaa37F", 5);
   const marketplace = await Marketplace.deployed();
   await deployer.deploy(RentableVehicles, marketplace.address);
 };
