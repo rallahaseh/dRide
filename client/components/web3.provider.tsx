cb871fde1e6f223974368ccae4d5089950e2b97d
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-11-26 20:53:40 +0100

    Remove unused networks

--- client/components/web3.provider.tsx
+++ client/components/web3.provider.tsx
@@ -4,26 +4,24 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { infuraProvider } from 'wagmi/providers/infura';
-import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-    [chain.goerli, chain.mainnet],
-    [
-      infuraProvider({ apiKey: process.env.INFURA_ID }),
-      publicProvider()
-    ]
-  );
-  
-  const { connectors } = getDefaultWallets({
-    appName: 'dRide | Decentralized Ride',
-    chains
-  });
-  
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider
-  });
+  [chain.goerli],
+  [
+    infuraProvider({ apiKey: process.env.INFURA_ID })
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'dRide | Decentralized Ride',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
 
 export const Web3Provider: FC<Web3ProviderProps> = (props: Web3ProviderProps) => {
   const { children } = props;
