fbb1ae79e66913f8fd840319e3e0c88a8f00f870
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-12-01 18:48:31 +0100

    Update page view adding date

--- client/pages/rentVehicle/index.tsx
+++ client/pages/rentVehicle/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { NextPage } from 'next';
 import { SearchResult, SearchVehicle } from './search';
 import { Vehicle } from './rental-vehicles';
-import Box from '@mui/material/Box';
+import { Box, Grid, Paper, Alert } from '@mui/material';
 
 enum State {
   search,
@@ -24,9 +24,31 @@ const IndexPage: NextPage = () => {
     );
   }
 
+  const getDateFormatted = (unixTimestamp: number) => {
+    const date = new Date(unixTimestamp * 1000);
+    return date.toLocaleDateString()
+  }
+
   const VehiclesComponent = () => {
     return (
-      <Vehicle result={searchResult} />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid container item spacing={3}>
+            <Grid item xs={16}>
+              <Paper>
+                <Alert severity="info">
+                  {"List of available vehicles from " + getDateFormatted(searchResult!.date.from!) + " - " + getDateFormatted(searchResult!.date.to!)}
+                </Alert>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container item spacing={3}>
+            <Grid item xs={16}>
+              <Vehicle result={searchResult} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
 
