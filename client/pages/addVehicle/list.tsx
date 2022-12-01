acc03b8fe8da77966d0f33e07ca20c7a99fa1165
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2022-12-01 21:59:17 +0100

    Add feedback for response

--- client/pages/addVehicle/list.tsx
+++ client/pages/addVehicle/list.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, Fragment, useState } from 'react';
 // MUI Components
 import {
     Box,
@@ -7,7 +7,9 @@ import {
     Checkbox,
     Button,
     CircularProgress,
-    Typography
+    Typography,
+    Backdrop,
+    Alert
 } from '@mui/material';
 // Contracts
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi';
@@ -21,9 +23,20 @@ interface ListVehicleProps {
 
 const ListVehicle: FC<ListVehicleProps> = (props: ListVehicleProps) => {
     const [checked, setChecked] = useState(false);
+    const [openBackdrop, setOpenBackdrop] = useState(false);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setChecked(event.target.checked);
     };
+    const handleCloseBackdrop = () => {
+        setOpenBackdrop(false);
+    };
+    const handleToggleBackdrop = () => {
+        setOpenBackdrop(!openBackdrop);
+    };
+    const handleClosePage = () => {
+        setOpenBackdrop(false);
+        props.onComplete();
+    }
 
     // Web3
     // List an NFT
@@ -51,14 +64,18 @@ const ListVehicle: FC<ListVehicleProps> = (props: ListVehicleProps) => {
         isLoading: isListNFTTransactionLoading
     } = useWaitForTransaction({
         hash: listNFTRequestData?.hash,
-        onError: () => { console.log("listNFT - Error: ", listNFTTransactionData); },
+        onError: () => {
+            console.log("listNFT - Error: ", listNFTTransactionData);
+            handleToggleBackdrop();
+        },
         onSuccess: () => {
             console.log("listNFT - Success: ", listNFTTransactionError);
-            props.onComplete();
+            handleToggleBackdrop();
         }
     });
 
-    const onClickHanlder = async () => {
+    const onMintClickHanlder = async () => {
+        setOpenBackdrop(false);
         await listNFT?.();
     }
 
@@ -77,36 +94,61 @@ const ListVehicle: FC<ListVehicleProps> = (props: ListVehicleProps) => {
         );
     }
     return (
-        <Box
-            sx={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}
-        >
-            <Grid item xs={12}>
-                <FormControlLabel
-                    label="Do you agree to add your vehicle to the list?"
-                    control={
-                        <Checkbox
-                            checked={checked}
-                            onChange={handleChange}
-                            inputProps={{ 'aria-label': 'controlled' }}
-                        />
-                    }
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <Button
-                    variant="contained"
-                    disabled={!checked}
-                    onClick={onClickHanlder}
+        <>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={openBackdrop}
+            >
+                {listNFTTransactionError &&
+                    <Grid container spacing={2} md={8} alignContent="center" justifyContent="center">
+                        <Grid item xs={16} md={8}>
+                            <Alert severity="error">An error has occurred, please try again</Alert>
+                        </Grid>
+                        <Grid item xs={16} md={8}>
+                            <Button onClick={onMintClickHanlder} color="error">Try again</Button>
+                        </Grid>
+                    </Grid>
+                }
+                {listNFTTransactionData &&
+                    <>
+                        <Alert severity="success">Your vehicle Has been added successfully!</Alert>
+                        <Button onClick={handleClosePage} color="success">Done</Button>
+                    </>
+                }
+            </Backdrop>
+            {!openBackdrop &&
+                <Box
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }}
                 >
-                    Add to list
-                </Button>
-            </Grid>
-        </Box>
+                    <Grid item xs={12}>
+                        <FormControlLabel
+                            label="Do you agree to add your vehicle to the list?"
+                            control={
+                                <Checkbox
+                                    checked={checked}
+                                    onChange={handleChange}
+                                    inputProps={{ 'aria-label': 'controlled' }}
+                                />
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            variant="contained"
+                            disabled={!checked}
+                            onClick={onMintClickHanlder}
+                        >
+                            Add to list
+                        </Button>
+                    </Grid>
+                </Box>
+            }
+        </>
     );
 }
 
